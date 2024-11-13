f7fd7570d57865b8ee14a2ce7516a72b1160863b
naut74 <ngoctuan14032001@gmail.com>
2024-11-13 14:20:48 +0700

    Stop Watch

---
+++ com.tenCty.classAndOop/src/StopWatch/StopWatch.java
@@ -0,0 +1,59 @@
+package StopWatch;
+
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+    public StopWatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void stop() {
+        this.endTime = System.currentTimeMillis();
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public long getElapsedTime() {
+        return endTime - startTime;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[100000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100000);
+        }
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        selectionSort(array);
+
+        stopWatch.stop();
+        System.out.println("Thời gian thực thi của thuật toán sắp xếp chọn: " + stopWatch.getElapsedTime() + " ms");
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
